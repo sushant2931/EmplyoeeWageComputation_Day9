ebd662c99e7ec7c9d037781539d6a2134d7814dd
sushant2931 <sushantlad8888@gmail.com>
2021-09-02 23:55:47 +0530

    Check Emplyoee Present OR Absent

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,6 +1,22 @@
 
 public class EmployeeWageMain {
+
+	final int IS_PRESENT = 1;
+
+	public void checkIsPresent(int empCheck) {
+		if (empCheck == IS_PRESENT)
+			System.out.println("\nEmployee is Present");
+		else
+			System.out.println("\nEmployee is Absent");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+		EmployeeWageMain wage = new EmployeeWageMain();
+
+		int empCheck = (int) (Math.random() * 10) % 2;
+		wage.checkIsPresent(empCheck);
+
 	}
 }
